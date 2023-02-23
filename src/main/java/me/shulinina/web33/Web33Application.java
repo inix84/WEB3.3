62c73291a221e4a964f370b58dec8b03aba5162f
inix84 <inix84@mail.ru>
2023-02-24 04:20:34 +0500

    3 коммит

--- src/main/java/me/shulinina/web33/Web33Application.java
+++ src/main/java/me/shulinina/web33/Web33Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Web33Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Web33Application.class, args);
     }
 }
